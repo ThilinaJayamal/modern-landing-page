9c773a6fc84c7e73acf184e84c9523a7fa5ce4b3
ThilinaJayamal <thilinajayamal271@gmail.com>
2024-09-25 09:44:43 +0530

    header scroll effect added

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,23 +1,38 @@
 import clsx from 'clsx';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link as LinkScroll } from 'react-scroll'
-
-const NavLink = ({ title = "" }) => (
-  <LinkScroll className='base-bold text-p4 uppercase 
-  transition-colors duration-500  
-  cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5'>
-    {title}
-  </LinkScroll>
-)
 
 function Header() {
 
+  const [hasScrolled, setHasScrolled] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setHasScrolled(window.scrollY > 32);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+
+  }, [])
+
+  const NavLink = ({ title = "" }) => (
+    <LinkScroll onClick={()=>setIsOpen(false)} offset={-100} spy smooth to={title} className='base-bold text-p4 uppercase 
+    transition-colors duration-500  
+    cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5' activeClass='nav-active'>
+      {title}
+    </LinkScroll>
+  )
+
+
   return (
-    <header className='fixed top-0 left-0 z-50 w-full py-10'>
+    <header className={clsx('fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4', hasScrolled && 'py-2 bg-black-100/40 backdrop-blur-[8px]')}>
       <div className='container flex max-lg:px-5'>
-        <a href="" className='lg:hidden flex-1 z-2'>
+        <a href="" className='lg:hidden flex-1 z-2 cursor-pointer'>
           <img src="/images/xora.svg" alt="Logo" width={115} height={55} />
         </a>
 
@@ -32,13 +47,13 @@ function Header() {
               <ul className='flex max-lg:block max-lg:px-12'>
 
                 <li className='nav-li'>
-                  <NavLink title="Features" />
+                  <NavLink title="features" />
                   <div className='dot' />
                   <NavLink title={"Pricing"} />
                 </li>
 
                 <li className='nav-logo'>
-                  <LinkScroll to='hero' offset={-150} spy smooth className={clsx('max-lg:hidden transition-transform duration-500 cursor-pointer')}>
+                  <LinkScroll to='hero' offset={-250} spy smooth className={clsx('max-lg:hidden transition-transform duration-500 cursor-pointer')}>
                     <img src="/images/xora.svg" alt="Xora" width={160} height={55} />
                   </LinkScroll>
                 </li>
